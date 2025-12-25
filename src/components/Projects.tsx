943f087547852708732b0a32447096033837c660
Victor Ricardo Oliveira Nunes <victoroliv2004@gmail.com>
2025-12-25 01:36:06 -0300

    [feat] projects.tsx | Ajusta titulo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,25 +6,37 @@ import Image from 'next/image';
 const projects = [
   {
     type: 'Programming',
-    title: 'Programming Project 1',
-    description: 'A brief description of the programming project.',
-    image: '/project-placeholder.svg',
-  },
-  {
-    type: 'Data',
-    title: 'Data Project 1',
-    description: 'A brief description of the data project.',
-    image: '/project-placeholder.svg',
-  },
-  {
-    type: 'Programming',
-    title: 'Programming Project 2',
+    title: 'PyClass',
     description: 'A brief description of the programming project.',
     image: '/project-placeholder.svg',
   },
   {
     type: 'Programming',
-    title: 'Programming Project 3',
+    title: 'IEBI Membresia',
+    description: 'A brief description of the programming project.',
+    image: '/project-placeholder.svg',
+  },
+  {
+    type: 'Programming',
+    title: 'Mikrotik-SDWAN',
+    description: 'Another programming project.',
+    image: '/project-placeholder.svg',
+  },
+  {
+    type: 'Programming',
+    title: 'AskServiços',
+    description: 'Another programming project.',
+    image: '/project-placeholder.svg',
+  },
+  {
+    type: 'Programming',
+    title: 'Gerador QRCode (FGH)',
+    description: 'Another programming project.',
+    image: '/project-placeholder.svg',
+  },
+  {
+    type: 'Programming',
+    title: 'OdontoRAIZ',
     description: 'Another programming project.',
     image: '/project-placeholder.svg',
   },
@@ -51,8 +63,8 @@ const Projects = () => {
   const visibleProjects = projects.slice(startIndex, startIndex + projectsPerPage);
 
   return (
-    <div className="container mx-auto px-4 text-center">
-      <h2 className="text-3xl font-bold mb-4">Projects</h2>
+    <div className="container h-[400px] mx-auto px-4 text-center">
+      <h2 className="text-3xl font-bold mb-4">Projetos</h2>
       <hr className="w-16 h-1 mx-auto my-6 bg-gray-300 border-0 rounded" />
       <div className="relative">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">

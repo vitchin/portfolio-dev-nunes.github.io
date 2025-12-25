c7a771c9183079fd2c30f4d390d3e63850a4d55b
Victor Ricardo Oliveira Nunes <victoroliv2004@gmail.com>
2025-12-25 16:04:28 -0300

    [feat] faz com que o site carregue a formatação

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
